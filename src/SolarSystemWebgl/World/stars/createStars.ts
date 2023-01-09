85bd6137e59480ef6611eb03c792883cd398e4cd
eoldam <eoldamstory@gmail.com>
2023-01-09 21:01:29 +0900

    earth maps, saturn ring

--- src/SolarSystemWebgl/World/stars/createStars.ts
+++ src/SolarSystemWebgl/World/stars/createStars.ts
@@ -1,4 +1,15 @@
-import { Mesh, MathUtils, Texture, SphereGeometry, MeshBasicMaterial, MeshStandardMaterial } from "three";
+import {
+  Mesh,
+  MathUtils,
+  Texture,
+  SphereGeometry,
+  MeshBasicMaterial,
+  MeshStandardMaterial,
+  Vector2,
+  RingGeometry,
+  DoubleSide,
+  ObjectSpaceNormalMap,
+} from "three";
 import { Star } from "../../../types";
 import Loader from "../../systems/Loader";
 import { UNIT } from "./datas";
@@ -15,21 +26,41 @@ export default async function createStars(loader: Loader, starDatas: Array<Star>
   ];
   const textures: Array<Texture> = await Promise.all(promises);
 
-  console.log(textures);
-
   const getGeometry = (radius: number) => {
     return new SphereGeometry(radius, 32, 32);
   };
-  const getMaterial = (texture: Texture) => {
-    return new MeshStandardMaterial({
+  const getMaterial = (star: Star, texture: Texture) => {
+    const isEarth = star.name === "earth";
+    const baseOptions = {
       map: texture,
+    };
+    const earthOptions = {
+      map: texture,
+      normalMap: isEarth ? textures[7] : null,
+      displacementMap: isEarth ? textures[9] : null,
+      displacementScale: -0.04,
+    };
+    return new MeshStandardMaterial(isEarth ? { ...baseOptions, ...earthOptions } : baseOptions);
+  };
+
+  const addRing = (saturn: Mesh, radius: number) => {
+    const ringGeometry = new RingGeometry(radius + 1, radius * 2, 32, 32);
+    const ringMaterial = new MeshStandardMaterial({
+      map: textures[10],
+      side: DoubleSide,
     });
+    const ring = new Mesh(ringGeometry, ringMaterial);
+    ring.rotateX(-Math.PI / 2);
+    saturn.add(ring);
   };
 
   const stars = starDatas.map((sd, index) => {
     const radius = sd.radius / UNIT;
-    const star: Mesh = new Mesh(getGeometry(radius), getMaterial(textures[index]));
+    const star: Mesh = new Mesh(getGeometry(radius), getMaterial(sd, textures[index]));
     star.position.x += sd.distanceToSun;
+    if (sd.name === "saturn") {
+      addRing(star, radius);
+    }
     return star;
   });
 
